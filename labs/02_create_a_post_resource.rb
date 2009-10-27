feb6a9667dbc3270fe5c7b63d2ed76c2ab405286
Adam Bair <adambair@gmail.com>
2009-10-27 12:45:02 -0400

    Add lab 02

---
+++ labs/02_create_a_post_resource.rb
@@ -0,0 +1,7 @@
+# --------------------------
+# Lab 02: Create a Post Resource
+# --------------------------
+# Use the scaffold generator to create a Post resource
+# The Post should have a title, body, and author fields
+# Start your application and explore the freshly minted scaffold
+# --------------------------
---
+++ labs/solutions/02_create_a_post_resource.rb
@@ -0,0 +1,13 @@
+# --------------------------
+# Lab 02: Create a Post Resource
+# --------------------------
+# Use the scaffold generator to create a Post resource
+# The Post should have a title, body, and author fields
+# Start your application and explore the freshly minted scaffold at /posts
+# --------------------------
+
+# ruby script/generate Post title:string author:string body:text
+# rake db:migrate
+# ruby script/server
+# Open http://localhost:3000/posts in a browser
+# Explore!

1078499b1629f42911ceccaaa7c1281609c7efd6
Adam Bair <adambair@gmail.com>
2009-10-28 21:06:35 -0400

    Add blog solution

---
+++ labs/solutions/blog/app/controllers/comments_controller.rb
@@ -0,0 +1,54 @@
+class CommentsController < ApplicationController
+  before_filter :find_post
+  before_filter :find_comment, :only => [:show, :edit, :update, :destroy]
+  before_filter :authenticat, :only => [:edit, :destroy, :create]
+
+  def index
+    @comments = @post.comments.all
+  end
+
+  def show
+  end
+
+  def new
+    @comment = @post.comments.new
+  end
+
+  def edit
+  end
+
+  def create
+    @comment = @post.comments.new(params[:comment])
+
+    if @comment.save
+      flash[:notice] = 'Comment was successfully created.'
+      redirect_to(@comment.post)
+    else
+      render :action => "new"
+    end
+  end
+
+  def update
+    if @comment.update_attributes(params[:comment])
+      flash[:notice] = 'Comment was successfully updated.'
+      redirect_to(@comment)
+    else
+      render :action => "edit"
+    end
+  end
+
+  def destroy
+    @comment.destroy
+
+    redirect_to(comments_url)
+  end
+
+  private
+    def find_post
+      @post = Post.find(params[:post_id])
+    end
+
+    def find_comment
+      @comment = @post.comments.find(params[:id])
+    end
+end
---
+++ labs/solutions/blog/config/routes.rb
@@ -0,0 +1,8 @@
+ActionController::Routing::Routes.draw do |map|
+  map.resources :posts, :has_many => :comments
+
+  map.root :controller => "posts"
+
+  map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/:id.:format'
+end

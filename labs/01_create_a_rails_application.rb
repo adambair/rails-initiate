2bab3914fc8bf51167fa457f38651e7be68b2f1c
Adam Bair <adambair@gmail.com>
2012-03-26 11:48:14 -0400

    Update lab 01

--- labs/01_create_a_rails_application.rb
+++ labs/01_create_a_rails_application.rb
@@ -2,7 +2,7 @@
 # Lab 01: Create a Rails Application [ 01_create_a_rails_application.rb ]
 # --------------------------
 # Use the 'rails' command to create a new application callled 'blog'
-# Test your application by running 'ruby script/server' in the blog folder
+# Test your application by running 'rails server' in the blog folder
 # View your application in a web browser
 # Explore the directory structure of your newly created application
 # --------------------------
--- labs/solutions/01_create_a_rails_application.rb
+++ labs/solutions/01_create_a_rails_application.rb
@@ -1,13 +1,13 @@
 # --------------------------
 # Lab 01: Create a Rails Application [ 01_create_a_rails_application.rb ]
 # --------------------------
-# Use the 'rails' command to create a new application callled 'blog'
-# Test your application by running 'ruby script/server' in the blog folder
+# Use the 'rails' command to create a new application called 'blog'
+# Test your application by running 'rails server' in the blog folder
 # View your application in a web browser
 # Explore the directory structure of your newly created application
 # --------------------------
 
-# rails blog
+# rails new blog
 # cd blog
-# ruby script/server
+# rails server
 # Open http://localhost:3000 in a web browser

10f4ffb30c7952a2e16b51deab0e9c70b44c0447
Adam Bair <adambair@gmail.com>
2012-03-27 12:57:44 -0400

    Update lab 06

--- labs/06_extract_post_fields.rb
+++ labs/06_extract_post_fields.rb
@@ -1,8 +1,6 @@
 # --------------------------
 # Lab 06: Extract Post Form Fields [ 06_extract_post_form_fields.rb]
 # --------------------------
-# Create a partial that will contain extracted post form fields
-# Extract common form fields from app/views/posts/edit.html.erb and new.html.erb
-# Put the common fields into the partial
-# Make sure that the forms still function
+# Review the partial in app/views/posts/edit.html.erb and new.html.erb
+# Review the layout of the rails application
 # --------------------------
--- labs/solutions/06_extract_post_fields.rb
+++ labs/solutions/06_extract_post_fields.rb
@@ -1,32 +1,7 @@
 # --------------------------
-# Lab 06: Extract Post Form Fields [ 06_extract_post_form_fields.rb]
+# Lab 06: Examine Partials and Review [ 06_extract_post_form_fields.rb ]
 # --------------------------
-# Create a partial that will contain extracted post form fields
-# Extract common form fields from app/views/posts/edit.html.erb and new.html.erb
-# Put the common fields into the partial
-# Make sure that the forms still function
+# Review the partial in app/views/posts/edit.html.erb and new.html.erb
+# Review the layout of the rails application
 # --------------------------
 
-# create an empty file called _post_fields.erb
-# extract the following fields from app/views/posts/edit.html.erb and new.html.erb
-
-  <%= f.error_messages %>
-
-  <p>
-    <%= f.label :title %><br />
-    <%= f.text_field :title %>
-  </p>
-  <p>
-    <%= f.label :author %><br />
-    <%= f.text_field :author %>
-  </p>
-  <p>
-    <%= f.label :body %><br />
-    <%= f.text_area :body %>
-  </p>
-
-# In place of the extracted form fields render the partial
-
-  <%= render :partial => 'post_fields', :locals => {:f => f} %>
-
-# start your application and verify that the forms still function ;)

04000f330c8aaf31de80783fc4acd4bb7d1cf5cb
Adam Bair <adambair@gmail.com>
2012-03-30 17:01:03 -0400

    Update lab 08

--- labs/08_basic_auth.rb
+++ labs/08_basic_auth.rb
@@ -1,6 +1,6 @@
 # --------------------------
 # Lab 08: Add basic authentication [ 08_add_basic_authentication.rb ]
 # --------------------------
-# Protect the destroy and edit in the CommentsController with Basic Auth
+# Protect the destroy and edit actions in the CommentsController with Basic Auth
 # --------------------------
 
--- labs/solutions/08_basic_auth.rb
+++ labs/solutions/08_basic_auth.rb
@@ -1,28 +1,15 @@
 # --------------------------
 # Lab 08: Add basic authentication [ 08_add_basic_authentication.rb ]
 # --------------------------
-# Protect the destroy and edit in the CommentsController with Basic Auth
+# Protect the destroy and edit actions in the CommentsController with Basic Auth
 # Protect everything except show/index in the PostsController with Basic Auth
 # --------------------------
 
-# add a private method called 'authenticate' to ApplicationController
-
-    private
-      def authenticate
-        authenticate_or_request_with_http_basic do |user, pass|
-        user == "admin" && pass == "password"
-      end
-
-# add a before_filter to the CommentsController
+# add a authentication filter to the CommentsController
 
     class CommentsController < ApplicationController
-      before_filter :authenticate, :only => [:destroy, :edit]
-    end
-
-# add a before_filter to the CommentsController 
-
-    class PostsController < ApplicationController
-      before_filter :authenticate, :except => [:show, :index]
+      http_basic_authenticate_with :name => "admin", :password => "password", 
+        :only => [:destroy, :edit]
     end
 
 # test the protected actions in a browser to make sure they're... protected.

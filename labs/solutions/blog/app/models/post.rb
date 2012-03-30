f61402966a66e97d8c686840c24568b04f248b93
Adam Bair <adambair@gmail.com>
2012-03-30 17:01:31 -0400

    Remove old blog example

--- labs/solutions/blog/app/models/post.rb
+++
@@ -1,8 +0,0 @@
-class Post < ActiveRecord::Base
-  has_many :comments
-
-  validates_presence_of :title, :body, :message => 'ZOMG REQUIRED'
-
-  named_scope :by_date, :order => "created_at DESC"
-  named_scope :latest, lambda {{:conditions => ['created_at > ?', 10.minutes.ago]}}
-end
--- labs/solutions/blog/config/routes.rb
+++
@@ -1,8 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.resources :posts, :has_many => :comments
-
-  map.root :controller => "posts"
-
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
-end

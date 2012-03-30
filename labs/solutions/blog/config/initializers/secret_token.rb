874462a909400c9be1f656430f7c042e906d612b
Adam Bair <adambair@gmail.com>
2012-03-30 17:01:49 -0400

    Add 3.2 blog example app

---
+++ labs/solutions/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'c2b1447925970d961551755a1edd520ab9974b5c1d1717df033790cc1e0ab61aa421f9dd7ab7517f341fec527d47765369ef635c662ca9de87aaaf24f9d8fbcf'
